24ae3a10eff5e4d864df7b57cd78eb3bd17ff295
BeerAndCigarettes <gennadii.gorbun@gmail.com>
2021-09-21 18:13:10 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kouo',
+    version='1',
+    packages=['kouo_task'],
+    url='',
+    license='',
+    author='Gennadii',
+    author_email='',
+    description='Kouo Coding Task'
+)
