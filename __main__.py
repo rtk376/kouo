eb1f41c6658f88806f5623468f40da8cafbf28ad
BeerAndCigarettes <gennadii.gorbun@gmail.com>
2021-09-21 22:33:59 +0100

    Added some comments and split into functions

---
+++ __main__.py
@@ -0,0 +1,22 @@
+import argparse
+from lib import print_top_starred_repos
+
+if __name__ == '__main__':
+    ### Initialising the command line argument parser
+    parser = argparse.ArgumentParser(description='Input a GitHub username to get top 5 starred public repositories')
+    parser.add_argument('-u', '--username', type=str, nargs=1, dest='username', help='Github username')
+    parser.add_argument('-t', '--top', type=int, default=[5], nargs=1, dest='num_top',
+                        help='Number of top starred repositories shown')
+
+    ### Parsing the arguments
+    try:
+        args = parser.parse_args()
+    except argparse.ArgumentError:
+        print(parser.print_help())
+
+    username = args.username[0]
+    num_top = args.num_top[0] #how many repos to show
+
+    ### Print top num_top starred repos
+    print_top_starred_repos(username, num_top)
+
--- lib.py
+++ lib.py
@@ -1,12 +1,10 @@
 from github import Github, GithubException
 
-
-def print_top_starred_repos(username, num_top):
+def get_user(username):
     '''
-    This
+    This function retrieves the user object corresponding to the required username, if such user exists
     :param username:
-    :param num_top:
-    :return:
+    :return: Github.NamedUser
     '''
     g = Github()  # instantiating main class
     try:
@@ -15,15 +13,27 @@ def print_top_starred_repos(username, num_top):
         print('Specified user does not exist, please enter a different username')
         exit()
 
+    return user
+
+def print_top_starred_repos(username, N):
+    '''
+    This function prints top N starred repositories of a given user on GitHub, if the user exists and has starred repos.
+    :param username:
+    :param N:
+    :return:
+    '''
+
+    user = get_user(username)
     user_repos = user.get_repos()
 
-    rns = []  # list to store repository names and their star count
+    rns = []  # list to store repository names and their star count as [repo_name, star_count]
 
     if user_repos.totalCount == 0:
         print('Specified user does not have any public repositories')
         exit()
 
     for repo in user_repos:
+        # only append starred repos, otherwise "top N" makes no sense
         if repo.stargazers_count > 0:
             rns.append([repo.name, repo.stargazers_count])
 
@@ -32,6 +42,7 @@ def print_top_starred_repos(username, num_top):
         exit()
 
     sorted_rns = sorted(rns, key=lambda x: x[1], reverse=True)  # sorted list of repos with starcount (highest first)
-    print(f'Top {num_top} starred repositories:')
-    for repo in sorted_rns[:num_top]:
+    print(f'Top {min(N, len(sorted_rns))} starred repositories:')
+    # printing top N starred repos, or as many as the user has if < N
+    for repo in sorted_rns[:N]:
         print(repo[0], repo[1])
